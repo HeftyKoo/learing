7568ae01502ef09b8c6eb314d853f0841c7f3d03
yeyuqiudeng <yeyuqiudeng@gmail.com>
2018-02-01 22:56:13 +0800

    node.js in practice: chapter7

---
+++ node/nodejs_in_practice/chapter7/proxy.js
@@ -0,0 +1,23 @@
+var http = require('http')
+var url = require('url')
+
+http.createServer(function (req,res) {
+  console.log('start request:', req.url)
+  var options = url.parse(req.url)
+  options.headers = req.headers
+  var proxyRequest = http.request(options, function (proxyResponse) {
+    proxyResponse.on('data', function (chunk) {
+      res.write(chunk, 'binary')
+    })
+    proxyResponse.on('end', function () {
+      res.end()
+    })
+    res.writeHead(proxyResponse.statusCode, proxyResponse.headers)
+  })
+  req.on('data', function (chunk) {
+    proxyRequest.write(chunk, 'binary')
+  })
+  req.on('end', function () {
+    proxyRequest.end()
+  })
+}).listen(8080)
